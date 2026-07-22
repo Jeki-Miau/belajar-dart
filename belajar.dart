9388d7869de8fcfb37836ff877e161400c1734b3
Jeki <dzakyabdh@gmail.com>
2026-07-22 07:59:29 +0700

    aku ngisi wow

--- belajar.dart
+++ belajar.dart
@@ -1,3 +1,11 @@
-void main() { 
-  print("Hello, World!");
+void main() {
+  String nama = 'Jeki wow';
+  int umur = 1100;
+  double tinggi = 190.9;
+  bool sudahLulus = false;
+
+  print('Nama: $nama'); 
+  print('Umur: $umur tahun');
+  print('Tinggi: $tinggi cm');
+  print('Sudah Lulus: $sudahLulus');
 }
